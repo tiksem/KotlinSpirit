777a72737a105ddabf96dde31927ee38c9b7f7f5
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2022-09-14 15:17:36 +0200

    CharPredicateRuleTest not tests are added

--- app/src/test/java/com/example/kotlinspirit/CharPredicateRuleTest.kt
+++ app/src/test/java/com/example/kotlinspirit/CharPredicateRuleTest.kt
@@ -75,4 +75,74 @@ class CharPredicateRuleTest {
         Assert.assertEquals(r.tryParse("\\"), null)
         Assert.assertEquals(r.tryParse("|"), 1)
     }
+
+    @Test
+    fun noTest1() {
+        val r = (!char('a', 'b', 'e')).compile()
+        Assert.assertEquals(r.tryParse("adf"), null)
+        Assert.assertEquals(r.tryParse("edf"), null)
+        Assert.assertEquals(r.tryParse("bde"), null)
+        Assert.assertEquals(r.tryParse("eee"), null)
+        Assert.assertEquals(r.tryParse("uee"), 1)
+    }
+
+    @Test
+    fun noTest2() {
+        val r = (!char('a'..'u')).compile()
+        Assert.assertEquals(r.tryParse("xyz"), 1)
+        Assert.assertEquals(r.tryParse("dfgjdfhjghj"), null)
+        Assert.assertEquals(r.tryParse("zzzzzzzz"), 1)
+    }
+
+    @Test
+    fun noTest3() {
+        val r = (!char('a'..'u', 'e'..'r')).compile()
+        Assert.assertEquals(r.tryParse("xyz"), 1)
+        Assert.assertEquals(r.tryParse("dfgjdfhjghj"), null)
+        Assert.assertEquals(r.tryParse("zzzzzzzz"), 1)
+    }
+
+    @Test
+    fun noTest4() {
+        val r = (!char('z'..'a')).compile()
+        Assert.assertEquals(r.tryParse("xyz"), 1)
+        Assert.assertEquals(r.tryParse("dfgjdfhjghj"), 1)
+        Assert.assertEquals(r.tryParse("zzzzzzzz"), 1)
+    }
+
+    @Test
+    fun noTest5() {
+        val r = (!char(ranges = arrayOf('z'..'a'), chars = charArrayOf('a'))).compile()
+        Assert.assertEquals(r.tryParse("ayz"), null)
+        Assert.assertEquals(r.tryParse("dfgjdfhjghj"), 1)
+        Assert.assertEquals(r.tryParse("zzzzzzzz"), 1)
+    }
+
+    @Test
+    fun noTest6() {
+        val r = (!char(ranges = arrayOf('a'..'z'), chars = charArrayOf('a'))).compile()
+        Assert.assertEquals(r.tryParse("ayz"), null)
+        Assert.assertEquals(r.tryParse("dfgjdfhjghj"), null)
+        Assert.assertEquals(r.tryParse("zzzzzzzz"), null)
+    }
+
+    @Test
+    fun noTest7() {
+        val r = (!charIf {
+            it.isDigit()
+        }).compile()
+
+        Assert.assertEquals(r.tryParse("ayz"), 1)
+        Assert.assertEquals(r.tryParse("5ayz"), null)
+    }
+
+    @Test
+    fun noTest8() {
+        val r = (!char(ranges = arrayOf('a'..'z', '0'..'3', 'e'..'d'), chars = charArrayOf('|'))).compile()
+        Assert.assertEquals(r.tryParse("ayz"), null)
+        Assert.assertEquals(r.tryParse("dfgjdfhjghj"), null)
+        Assert.assertEquals(r.tryParse("zzzzzzzz"), null)
+        Assert.assertEquals(r.tryParse("\\"), 1)
+        Assert.assertEquals(r.tryParse("|"), null)
+    }
 }
