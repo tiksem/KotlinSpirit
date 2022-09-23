7df527b55623130ad299635cb4ddcdbbdf4b95cb
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2022-09-23 19:29:39 +0200

    one more optimization

--- app/src/main/java/com/kotlinspirit/core/ThreadSafeParser.kt
+++ app/src/main/java/com/kotlinspirit/core/ThreadSafeParser.kt
@@ -3,7 +3,9 @@ package com.kotlinspirit.core
 import java.util.concurrent.ConcurrentHashMap
 
 internal class ThreadSafeParser<T : Any>(originRule: Rule<T>): BaseParser<T>(originRule) {
-    private val ruleMap = ConcurrentHashMap<Thread, Rule<T>>()
+    private val ruleMap = ConcurrentHashMap<Thread, Rule<T>>().also {
+        it[Thread.currentThread()] = originRule
+    }
 
     override fun getRule(): Rule<T> {
         return ruleMap.getOrPut(Thread.currentThread()) {
